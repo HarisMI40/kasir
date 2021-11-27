149250eb63ce07779e15ce77120183d189b18c0a
HarisMI40 <harisMaulanaikhsan40@gmail.com>
2021-11-27 08:17:17 +0700

    tambah kolom softDelete pada product

--- app/Models/product.php
+++ app/Models/product.php
@@ -6,12 +6,14 @@ use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 
 class product extends Model
 {
     use Uuids;
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = ['kode_barang','nama_product', 'qty', 'harga'];
 
