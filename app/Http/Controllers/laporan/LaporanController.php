be1c127d44d883c36853b1fdbebfb52c6e56baa1
HarisMI40 <harisMaulanaikhsan40@gmail.com>
2021-12-01 17:39:11 +0700

    buat query sql laporan

--- app/Http/Controllers/laporan/LaporanController.php
+++ app/Http/Controllers/laporan/LaporanController.php
@@ -11,10 +11,21 @@ class LaporanController extends Controller
 {
     public function index()
     {
+        // $products = DB::table('penjualans')
+        //         ->selectRaw('tanggal , sum(total_qty) as total_barang_terjual , sum(total_harga) as total_pemasukan')
+        //         ->groupBy('tanggal')
+        //         ->get();
+
+        //  $products = DB::table('penjualans')
+        //         ->selectRaw('MONTH(created_at) as tanggal_data')
+        //         ->groupBy('tanggal_data')
+        //         ->get();
+
         $products = DB::table('penjualans')
-                ->selectRaw('tanggal , sum(total_qty) as total_barang_terjual , sum(total_harga) as total_pemasukan')
-                ->groupBy('tanggal')
-                ->get();
+        ->selectRaw('sum(total_qty)')
+        ->selectRaw('DATE_FORMAT(created_at, "%M") as tanggal_data')
+        ->groupBy('tanggal_data')
+        ->get();
 
         return $products;
         return view('laporan.data_laporan', compact('products'));
