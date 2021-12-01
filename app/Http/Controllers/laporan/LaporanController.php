a4ae5dd9ec64ad83fa71a3c40943d11eba2818b5
HarisMI40 <harisMaulanaikhsan40@gmail.com>
2021-12-01 17:49:48 +0700

    membersihkan kode laporanController

--- app/Http/Controllers/laporan/LaporanController.php
+++ app/Http/Controllers/laporan/LaporanController.php
@@ -11,16 +11,6 @@ class LaporanController extends Controller
 {
     public function index()
     {
-        // $products = DB::table('penjualans')
-        //         ->selectRaw('tanggal , sum(total_qty) as total_barang_terjual , sum(total_harga) as total_pemasukan')
-        //         ->groupBy('tanggal')
-        //         ->get();
-
-        //  $products = DB::table('penjualans')
-        //         ->selectRaw('MONTH(created_at) as tanggal_data')
-        //         ->groupBy('tanggal_data')
-        //         ->get();
-
         $products = DB::table('penjualans')
         ->selectRaw('sum(total_qty) as total_qty')
         ->selectRaw('sum(total_harga) as total_pendapatan')
@@ -28,7 +18,6 @@ class LaporanController extends Controller
         ->groupBy('tanggal_data')
         ->get();
 
-        // return $products;
         return view('laporan.data_laporan', compact('products'));
     }
 }
