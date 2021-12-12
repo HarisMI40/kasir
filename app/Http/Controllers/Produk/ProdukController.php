5221d589cd38015e6ddc7e9bf3f9b280f8736685
HarisMI40 <harisMaulanaikhsan40@gmail.com>
2021-12-12 13:13:20 +0700

    fix bug nama model produk

--- app/Http/Controllers/Produk/ProdukController.php
+++ app/Http/Controllers/Produk/ProdukController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\produk;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Product;
+use App\Models\product;
 
 class ProdukController extends Controller
 {
