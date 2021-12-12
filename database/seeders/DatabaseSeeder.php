96fb20323d688477ac416a1a1d83a8b3a62cd98c
HarisMI40 <harisMaulanaikhsan40@gmail.com>
2021-12-12 13:03:15 +0700

    tambah userSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ProductSeeder::class,
             PenjualanSeeder::class,
-            DetailPenjualanSeeder::class
+            DetailPenjualanSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -14,6 +16,15 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory()->count(2)->create();
+        // User::factory()->count(2)->create();
+        user::create(
+			[
+                'name' => 'admin2',
+                'email' => 'admin2@admin.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('admin123'), // password
+                'remember_token' => Str::random(10),
+            ]
+            );
     }
 }
