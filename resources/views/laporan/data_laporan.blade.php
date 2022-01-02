57c5822c4d78a485ef1e2c5ef09fea09eb94d3ab
HarisMI40 <harisMaulanaikhsan40@gmail.com>
2022-01-03 00:54:53 +0700

    menggunakan datatable versi cdn

--- resources/views/laporan/data_laporan.blade.php
+++ resources/views/laporan/data_laporan.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.index')
 
 @section('content')
+{{-- <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap5.min.css"> --}}
+{{-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.css"> --}}
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.11.3/af-2.3.7/b-2.1.1/b-colvis-2.1.1/b-html5-2.1.1/b-print-2.1.1/date-1.1.1/datatables.min.css"/>
 <div class="row">
     <div class="col-md-12">
-     <table class="table table-bordered table-hover table-sm">
+     <table  id="table_id" class="display table table-bordered table-hover table-sm">
              <thead>
                  <tr>
                      <th scope="col">No</th>
@@ -25,6 +28,50 @@
              @endforeach
              </tbody>
          </table>
+
+         {{-- <table id="table_id" class="display">
+            <thead>
+                <tr>
+                    <th>Column 1</th>
+                    <th>Column 2</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Row 1 Data 1</td>
+                    <td>Row 1 Data 2</td>
+                </tr>
+                <tr>
+                    <td>Row 2 Data 1</td>
+                    <td>Row 2 Data 2</td>
+                </tr>
+            </tbody>
+        </table> --}}
     </div>
 </div>
+@endsection
+
+@section('script')
+    <script src="{{asset('js/jquery-3.6.0.min.js')}}"></script>
+    {{-- <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script> --}}
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.11.3/af-2.3.7/b-2.1.1/b-colvis-2.1.1/b-html5-2.1.1/b-print-2.1.1/date-1.1.1/datatables.min.js"></script>
+
+    
+    {{-- <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap5.min.js"></script> --}}
+
+    <script>
+        $(document).ready( function () {
+            $('#table_id').DataTable({
+                dom: 'B<"clear">lfrtip',
+                buttons: true,
+                buttons: {
+                    name: 'primary',
+                    buttons: [ 'copy', 'excel', 'pdf' ]
+                }
+            });
+        } );
+    </script>
 @endsection
