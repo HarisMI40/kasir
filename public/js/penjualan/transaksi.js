bded4e9a909a8768ea6328c06656ada40be534b4
HarisMI40 <harisMaulanaikhsan40@gmail.com>
2022-01-03 14:07:05 +0700

    modularisasi halaman penjualan

--- public/js/penjualan/transaksi.js
+++ public/js/penjualan/transaksi.js
@@ -1,6 +1,6 @@
 const table = document.querySelector("table");
-
-table.addEventListener('click', function(event){
+    console.log(formatRupiah('1000'));
+	table.addEventListener('click', function(event){
     // console.log(event.target.tagName);
     if (event.target.tagName === "TD"){
         let id = event.target.getAttribute("data-id");
@@ -19,7 +19,7 @@ table.addEventListener('click', function(event){
 						}
 					});
 					event.preventDefault();
-					var id_penjualan_terakhir = jQuery("#penjualanTerakhir").val();
+					var id_penjualan_terakhir = jQuery("#id_penjualan").val();
 					var type = "POST";
 					var ajaxurl = 'penjualan/add/'+id_penjualan_terakhir+'/'+id;
 					$.ajax({
@@ -27,28 +27,58 @@ table.addEventListener('click', function(event){
 						url: ajaxurl,
 						dataType: 'json',
 						success: function (data) {
+							let detailPenjualan = data.data.detail_penjualan;
 							// alert('success');
                             console.log('success');
                             console.log(data);
+							console.log(detailPenjualan);
                             $("#table_nota").html("");
                             
-                            data.detail_penjualan.forEach(detail => {
+							// menampilkan produk yang dibelis
+                            detailPenjualan.forEach(detail => {
                                 $("#table_nota").append(`
-                                <td width="40%">${detail.product.nama_product}</td>
-                                <td width="10%">${detail.qty}</td>
-                                <td width="25%">${detail.sub_total}</td>
-                                <td width="20%">${detail.product.harga * detail.qty}</td>
-                                <td width="5%">
-                                        <button type="submit" class="btn btn-danger btn-sm"
-                                        ><i class="bi bi-trash" style="color:white"></i></button>
-                                </td>
-                            `);
+									<tr>
+										<td width="40%">${detail.product.nama_product}</td>
+										<td width="10%">${detail.qty}</td>
+										<td width="25%">${detail.sub_total}</td>
+										<td width="20%">${detail.product.harga * detail.qty}</td>
+										<td width="5%">
+												<button type="submit" class="btn btn-danger btn-sm"
+												><i class="bi bi-trash" style="color:white"></i></button>
+										</td>
+									</tr>
+								`);
+
+								// update quantity di table produk
+								$("td[data-id='"+detail.product.kode_barang+"']#qtyProduct").text(detail.product.qty)
+							
                             });
-                            
+
+							// menampilkan table total
+							$("#div_table_total").removeClass('d-none');
+							$("#table_total").html(`
+								<tr>
+									<td><strong>qty</strong></td>
+									<td>: ${data.data.total_qty}</td>
+								</tr>
+								<tr>
+									<td><strong>total</strong></td>
+									<td>: ${data.data.total_harga}</td>
+								</tr> 
+							`);
+							
+							// menampilkan button buy
+							$("#div_button_buy").removeClass("d-none");
+							// $("#div_button_buy").html(`
+							// 		<button type="submit" class="btn btn-success mt-4" id="buy">Buy</button>
+							// `);
+
+							
+
 						},
 						error: function (data) {
 							console.log(data);
-                            console.log('error');
+                            console.log('error : ' + data.message);
 						}
 					});
 			});
