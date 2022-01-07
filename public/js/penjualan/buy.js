0ab1b61038137947ff02268d3a1947f9ef5bf9c1
HarisMI40 <harisMaulanaikhsan40@gmail.com>
2022-01-08 00:25:16 +0700

    update file program javascript

--- public/js/penjualan/buy.js
+++ public/js/penjualan/buy.js
@@ -26,44 +26,44 @@
 							
 							$("#id_penjualan").val(data.id_penjualan);
 							
-							// // ==== Print =====
-							// let printer = "POS58 Printer(2)"; // setting nama printer
-							// let impresora = new Impresora(); // inisiasi
+							// ==== Print =====
+							let printer = "POS58 Printer(2)"; // setting nama printer
+							let impresora = new Impresora(); // inisiasi
 
-							// impresora.feed(1);
-							// impresora.setAlign("center");
-							// impresora.setFontSize(2, 2);
-							// impresora.write("TOKO Serba Ada \n");
-							// impresora.setFontSize(1, 1);
-							// impresora.write("Jl garnet, permata regency \n Cikampek Utara \n");
-							// impresora.write("--------------------------- \n");
-							// impresora.setAlign("left");
+							impresora.feed(1);
+							impresora.setAlign("center");
+							impresora.setFontSize(2, 2);
+							impresora.write("TOKO Serba Ada \n");
+							impresora.setFontSize(1, 1);
+							impresora.write("Jl garnet, permata regency \n Cikampek Utara \n");
+							impresora.write("--------------------------- \n");
+							impresora.setAlign("left");
 							let no = 1;
 
-							// data_product.forEach(dataProduct => {
-							// 	impresora.write(no + ". "+dataProduct.product.nama_product+" \n");
-							// 	impresora.write("   "+formatRupiah(dataProduct.product.harga)+" x "+dataProduct.qty+" "+formatRupiah(dataProduct.sub_total)+" \n");
-							// 	// console.log(formatRupiah(dataProduct.product.harga));
-							// 	// console.log(formatRupiah(dataProduct.sub_total));
-							// });
+							data_product.forEach(dataProduct => {
+								impresora.write(no + ". "+dataProduct.product.nama_product+" \n");
+								impresora.write("   "+formatRupiah(dataProduct.product.harga)+" x "+dataProduct.qty+" "+formatRupiah(dataProduct.sub_total)+" \n");
+								// console.log(formatRupiah(dataProduct.product.harga));
+								// console.log(formatRupiah(dataProduct.sub_total));
+							});
 							
-								// impresora.setAlign("center");
-								// impresora.write("--------------------------- \n");
-								// impresora.setAlign("right");
-								// impresora.write("Total : "+formatRupiah(data.data_product.total_harga)+" \n");
+								impresora.setAlign("center");
+								impresora.write("--------------------------- \n");
+								impresora.setAlign("right");
+								impresora.write("Total : "+formatRupiah(data.data_product.total_harga)+" \n");
 
-								// impresora.feed(1);
-								// impresora.setAlign("center");
-								// impresora.setFontSize(1, 1);
-								// impresora.write("Terimakasih Telah Berbelanja \n");
+								impresora.feed(1);
+								impresora.setAlign("center");
+								impresora.setFontSize(1, 1);
+								impresora.write("Terimakasih Telah Berbelanja \n");
 
-								// impresora.imprimirEnImpresora(printer)
-								// 	.then(valor => {
+								impresora.imprimirEnImpresora(printer)
+									.then(valor => {
 										
-								// 	});
+									});
 
-								// document.querySelector("#nota").innerHTML="";
-								// window.location="{{route('penjualan')}}"
+								document.querySelector("#nota").innerHTML="";
+								window.location="{{route('penjualan')}}"
 						},
 						error: function (data) {
 							console.log(data);
